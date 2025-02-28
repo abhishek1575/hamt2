dcb2bcb28f5228a3aaf77c9cf467dbbe9f470708
abhishek1575 <abhishekkhobe20@gmail.com>
2025-02-28 11:26:30 +0530

    initial commit

---
+++ src/main/java/cstech/ai/hamt/HamtApplication.java
@@ -0,0 +1,13 @@
+package cstech.ai.hamt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cstech/ai/hamt/HamtApplicationTests.java
@@ -0,0 +1,13 @@
+package cstech.ai.hamt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
